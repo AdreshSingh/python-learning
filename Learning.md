235d2d32369c8008f9193af4172023363a5e2d8e
Devank Singh <46901709+AdreshSingh@users.noreply.github.com>
2025-02-13 20:30:44 +0530

    progress day 2
    learnt IDLE & Basic features

---
+++ Learning.md
@@ -0,0 +1,11 @@
+Interactive mode IDLE
+ easily run single line statement
+ old code can't be run
+ REPL
+
+	READ
+	EVALUATE
+	PRINT
+	LOOP
+Script Mode IDLE
+ always need to save file first and then run by F5.
---
+++ begin.py
@@ -0,0 +1,13 @@
+# Commments
+
+
+# to display output
+print("we are working script mode")
+
+print("we are learning Py" * 4)
+
+#calculation
+price=90
+pocket_money = 100
+
+print("After buying Apples, balance now:",(pocket_money - price ))
