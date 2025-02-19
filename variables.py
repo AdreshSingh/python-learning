c6a97c9ee9a9376600a77d050263db7272e34833
Devank Singh <46901709+AdreshSingh@users.noreply.github.com>
2025-02-19 19:57:16 +0530

    new file
    Variables file

---
+++ variables.py
@@ -0,0 +1,16 @@
+# Variables
+
+# are used to store the value refrence or memory location
+name = "devank singh" # 1mb
+
+
+print("we are learning with",name)
+
+
+print("we are learning with",name)
+
+print("we are learning with",name)
+
+print("we are learning with",name)
+
+print("we are learning with",name)
