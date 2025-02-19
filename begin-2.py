68e828c61d1d384d30079fca9335ae3f6b676adb
Devank Singh <46901709+AdreshSingh@users.noreply.github.com>
2025-02-19 19:58:38 +0530

    new file
    Comments topic

---
+++ begin-2.py
@@ -0,0 +1,32 @@
+# comments
+
+## learning basics
+## commented with Alt + 3
+## uncomment with Alt + 4
+## print("i am simply learning basic of Py")
+
+# displaying output
+print("we are again printing")
+
+# single line comments
+
+# multi line comments
+'''
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+'''
+
+
+"""
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+print("we are again printing")
+"""
+
