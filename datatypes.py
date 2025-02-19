ab2aced7de85e5d2e808657bb7f739eb3deab7b7
Devank Singh <46901709+AdreshSingh@users.noreply.github.com>
2025-02-19 19:59:44 +0530

    new file
    Started Datatypes topic part -1

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+# Variables
+a = 10  # int or Integer
+# Datatypes
+# the type of value that is being stored in a variable.
+'''
+1. Primitive -> already comes defined by language
+    1. number -> Integer (int), Float (float), Complex (complex)
+    2. string -> str   # start from here
+    3. squence -> List, Tuple
+    4. Map -> Dictionary
+    5. Byte -> Bytes, BytesArray, MemoryView
+    6. Sets -> Set, Frozenset
+2. Non-Primitive
+'''
+# we can use python type() to confirm a varaible datatype
+print(type(a))
+
+a = 3.14
+print(type(a))
+
+a = 3+4j
+print(type(a))
+
+
+a = '''learn
+python 3
+'''
+
+a = """learn
+python 3
+"""
+print(type(a))
+print(a)
+
+
+myname = "devank"
+print(myname[5])
+
+
